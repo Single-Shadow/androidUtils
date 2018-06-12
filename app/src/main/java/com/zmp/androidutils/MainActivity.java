ee44860c26b0245a116e83131189f914acc3ef20
zhangmingping <mingping.zhang@uurobot.cn>
2018-06-12 11:04:35 +0800

    项目初始化化

---
+++ app/src/main/java/com/zmp/androidutils/MainActivity.java
@@ -0,0 +1,13 @@
+package com.zmp.androidutils;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+        }
+}
